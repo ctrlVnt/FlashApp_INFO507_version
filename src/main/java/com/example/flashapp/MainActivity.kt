6d042402d473a30a85bed16d51d176a465efebf2
ctrlVnt <giordanobruno227@gmail.com>
2022-10-16 23:38:32 +0200

    change icon launch

--- src/main/java/com/example/flashapp/MainActivity.kt
+++ src/main/java/com/example/flashapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.flashapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val add_button = findViewById<Button>(R.id.add_button)
+
+        add_button.setOnClickListener {
+            val intent = Intent(this, PopupMain::class.java)
+            intent.putExtra("popuptitle", "Error")
+            intent.putExtra("popuptext", "Sorry, that email address is already used!")
+            intent.putExtra("popupbtn", "OK")
+            intent.putExtra("darkstatusbar", false)
+            startActivity(intent)
+        }
     }
 }
